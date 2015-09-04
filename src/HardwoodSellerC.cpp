001ca7b497645291477a87c3dd90f329f89df077
linghan zhang <linghanzhangkate@gmail.com>
2015-09-04 01:37:19 -0400

    Complete Hardwood Seller program
    Just ran HardwoodSellerC.cpp, and 1.txt is an example of orders

--- src/HardwoodSellerC.cpp
+++ src/HardwoodSellerC.cpp
@@ -4,29 +4,139 @@
 // Version     : 1
 // Copyright   : It's free
 // Description : Main class for the Hardwood Seller
+// Editor: Linghan Zhang
 //============================================================================
 
 #include <iostream>
+#include <cstring>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+
+#include "WoodItem.h"
+
 using namespace std;
 
-int main() {
-	cout
-			<< "This is an empty main method"
-			<< endl;
+
+vector<string> InputItem;
+
+void readInputFile(FILE *fd) ;
+void checkInput(vector<string> Input);
+void orderMake(string wood, double price, double time, int weight);
+double deliveryTime(int weight,double base);
+
+int main(int argc, char *argv[]) {
+	FILE *fd;
+    if(argc != 2)
+	{
+		cerr<<"please input the filename."<<endl;
+		exit(1);
+	}
+	fd=fopen(argv[1],"r");
+	if(fd == NULL)
+	{
+		cerr<<"cannnot open the file."<<endl;
+		exit(1);
+	}
+	readInputFile(fd);
+	checkInput(InputItem);
 	return 0;
 }
 
 /*
  * Method to read the input file
  */
-void readInputFile(string inputFilePath) {
+void readInputFile(FILE *fd) 
+{
+	char buf[100];
+	char *tok;
+ 
+	while(fgets(buf,100,fd))
+	{
+		tok = strtok(buf,";:");
+		while(tok != NULL)
+		{
+			//cout<<tok<<endl;
+			InputItem.push_back(tok);
+			tok = strtok(NULL, ";:");
+		}
+	}
+	fclose(fd);	
+}
 
+void checkInput(vector<string> Input)
+{
+	int i;
+	int weight;
+	char *trk;
+	char buf[100];
+	string IDinfo;
+	WoodItem item;
+	
+	if(Input.size() < 4)
+	{
+		cerr<< "didn't input enough information, please follow the input format strictly." << endl;
+	    exit(1);
+	}
+	else
+	{
+        for(i=0; i < 2; i++)
+		{
+			IDinfo += Input[i];
+			IDinfo +=";";
+		}
+		IDinfo = IDinfo + Input[2];
+		cout<<IDinfo;
+		for(i=3; i < Input.size(); i = i+2)
+		{
+			WoodItem item(Input[i]);
+			strcpy(buf, Input[i+1].c_str());
+			sscanf(buf,"%d",&weight);
+			//cout<<"------------------------"<<endl;
+			//cout<<item.type<<" "<<item.price<<" "<<item.baseDeliveryTime<<" "<<weight<<endl;
+			orderMake(item.type, item.price, item.baseDeliveryTime, weight);
+		}     		 			 
+	}
+}
+
+void orderMake(string wood, double price, double time, int weight)
+{
+	double totalprice;
+	double totaltime = deliveryTime(weight,time);
+	totalprice = price * weight;
+	cout<< wood << ":";
+	cout<<weight<<"BF "<<price<<"dollars/BF "<< totaltime << "hours "<<totalprice<<"dollars "<<endl;
 }
 
 /*
  * Method to compute the deliveryTime
  */
-double deliveryTime() {
-	double deliveryETA = 0.0;
-	return deliveryETA;
+double deliveryTime(int weight,double base) {
+	double dTime;
+	if(weight >=1 && weight <=100)
+		dTime = base;
+	else if(weight >= 101&&weight <= 200)
+		dTime = 2*base;
+	else if(weight >= 201 && weight <= 300)
+		dTime = 3*base;
+	else if(weight >= 301 && weight <= 400)
+		dTime = 4*base;
+	else if(weight >= 401 && weight <= 500)
+		dTime = 5*base;
+	else if(weight >= 501 && weight <= 1000)
+		dTime = 5.5*base;
+	else if(weight < 1)
+	{
+		cerr<<"please check the amount of wood: too few." << endl;
+	    exit(1);
+	}  
+	else
+	{
+		cerr<<"please check the amount of wood: too much." << endl;
+	    exit(1);
+	}
+	return dTime;
 }
+
+
--- src/WoodItem.h
+++ src/WoodItem.h
@@ -3,6 +3,7 @@
  *
  *  Created on: Aug 24, 2015
  *      Author: Esteban
+ *  Editor: Linghan Zhang
  */
 
 
@@ -10,6 +11,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <stdlib.h>
 
 #ifndef WOODITEM_H_
 #define WOODITEM_H_
@@ -24,8 +26,62 @@ public:
 
 	// Member functions declaration
 	WoodItem();
-	WoodItem(std::string woodType, double deliveryTime, double itemPrice);
-
+	WoodItem(std::string woodType);
 };
+#include "WoodItem.h"
+#include <iostream>
+#include <sstream>
 
+using namespace std;
+/*
+ * Empty constructor
+ */
+WoodItem::WoodItem(){
+	type = "";
+	baseDeliveryTime = 0.0;
+	price = 0.0;
+}
+
+/*
+ * Method to initialize a tic tac toe game
+ * Initializes board and turn
+ */
+WoodItem::WoodItem(std::string woodType) {
+	type = woodType;
+	if(type == "Cherry")
+	{
+		baseDeliveryTime = 2.5;
+        price = 5.95;		
+	}
+	else if(type == "Curly Maple")
+	{
+		baseDeliveryTime = 1.5;
+		price = 6.00;
+	}
+	else if(type == "Genuine Mahogany")
+	{
+		baseDeliveryTime = 3;
+		price = 9.60;
+	}
+	else if(type == "Wenge")
+	{
+		baseDeliveryTime = 5;
+		price = 22.35;
+	}
+	else if(type == "White Oak")
+	{
+		baseDeliveryTime = 2.3;
+		price = 6.70;
+	}
+	else if(type == "Sawdust")
+	{
+		baseDeliveryTime = 1;
+		price = 1,5;
+	}
+	else
+	{
+		cerr<<"Fail to create this wooditem: we don't provide that kind of wood now, please check later." <<endl;
+		exit(-1);
+	}
+}
 #endif /* WOODITEM_H_ */
